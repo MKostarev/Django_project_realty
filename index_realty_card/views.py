36fba11b719891177747376ca97186c308f18287
Mikhail Kostarev <117512786+MKostarev@users.noreply.github.com>
2023-07-14 00:42:43 +0300

    commit

--- index_realty_card/views.py
+++ index_realty_card/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
 from realty.models import Realty
+from request.models import Rerust
 
 
 # Create your views here.
 
 def realty_detail_view(request, id):
     realty_card = Realty.objects.get(id=id)
-
-
-
     return render(request, 'realty_card.html', {'realty_card': realty_card})
+
+#class IndexBaseView (CreateView):
+#    template_name = 'realty_card.html'
+#    model = Rerust
+#    fields = ('name', 'email', 'question')
+#    success_url = '/index_home'
+
+
+
+#class Realty_detail_view (ListView):
+#    template_name = 'index.html'
+#    realty_card = Realty.objects.get(id=id)
+#    model = Realty
+#    context_object_name = 'all_realty'
 
--- realty/models.py
+++ realty/models.py
@@ -18,3 +18,10 @@ class Category_realty(models.Model):
 
     def __str__(self):
         return self.cat
+
+#class Managers_realty(models.Model):
+#    name = models.CharField(max_length=100)
+#    telefon = models.CharField(max_length=15)
+#    email = models.EmailField()
+#    cat = models.ForeignKey('Category_managers', on_delete=models.PROTECT)
+#    img = models.ImageField(upload_to='img', blank=True, null=True)
